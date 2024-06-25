196fc1fbb958d7840eda33568f2cece85c9351f6
Judah Almanzor <154279144+BrwnStallion@users.noreply.github.com>
2024-06-24 17:31:17 -0700

    feat: add Section implementation for cv

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,22 @@
+import React from "react";
 import { useState } from "react";
 import "./App.css";
+import Section from "./components/Section.jsx";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1>Vite + React</h1>
+      <h1>CV Generator</h1>
+      <Section title="General Information"></Section>
+      <Section title="Educational Experience"></Section>
+      <Section title="Professional Experience"></Section>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
