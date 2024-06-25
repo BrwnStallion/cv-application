2bb2a2b12879ff30074e25eb1b5080bc7f2da16c
Judah Almanzor <154279144+BrwnStallion@users.noreply.github.com>
2024-06-24 18:39:22 -0700

    feat: add test component for text input

---
+++ src/components/Input.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { useState } from "react";
+
+function Input({ type }) {
+  return (
+    <>
+      <label>Test</label>
+      <input type="text"></input>
+    </>
+  );
+}
+
+export { Input };
