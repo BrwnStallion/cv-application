99af3d8c7999f23cbf522ef85e122394c3e2a2d6
Judah Almanzor <154279144+BrwnStallion@users.noreply.github.com>
2024-06-25 10:34:37 -0700

    first commit

---
+++ src/data/placeholders.js
@@ -0,0 +1,21 @@
+const placeholders = [
+  {
+    sectionTitle: "General Information",
+    placeholders: ["John Doe", "john.doe@gmail.com", "111-222-3333"],
+  },
+  {
+    sectionTitle: "Educational Experience",
+    placeholders: [
+      "University of California, Berkeley",
+      "Bachelor of Science",
+      "Bioengineering",
+      "YYYY",
+    ],
+  },
+  {
+    sectionTitle: "Professional Experience",
+    placeholders: ["Starbucks", "Store Manager", "MM/DD/YYYY", "MM/DD/YYYY"],
+  },
+];
+
+export { placeholders };
