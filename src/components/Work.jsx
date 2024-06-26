9ea81c63753969f7f1f4e19a9280e58c8b62e488
Judah Almanzor <154279144+BrwnStallion@users.noreply.github.com>
2024-06-25 18:00:09 -0700

    feat: add instance/section creation logic

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,6 +1,49 @@
 import React from "react";
 import { useState } from "react";
+import { Input } from "./Input";
+import { InstanceButton } from "./Button";
 
-function WorkInstance() {}
+function WorkInstance() {
+  const [isCompleted, setIsCompleted] = useState(false);
 
-export { WorkInstance };
+  let workFields;
+  workFields = (
+    <div className="work-instance">
+      <Input type="text" subType="employer" parentSection="work" />
+      <Input type="text" subType="position" parentSection="work" />
+      <Input type="date" subType="startDate" parentSection="work" />
+      <Input type="date" subType="endDate" parentSection="work" />
+      <Input type="checkbox" parentSection="work" />
+    </div>
+  );
+
+  function handleClick(e) {
+    if (e.target.className === "completeEdit") {
+      setIsCompleted(!isCompleted);
+    }
+  }
+
+  return (
+    <>
+      {workFields}
+      <div className="section-buttons">
+        <InstanceButton
+          type="completeEdit"
+          onClick={handleClick}
+          isCompleted={isCompleted}
+        />
+        <InstanceButton type="remove" onClick={handleClick} />
+      </div>
+    </>
+  );
+}
+
+function WorkSection() {
+  return (
+    <>
+      <WorkInstance />
+      <InstanceButton type="addNew" />
+    </>
+  );
+}
+export { WorkInstance, WorkSection };
