6d29712bff0e8f2e6358ac2eba3331ed3d64aed4
Judah Almanzor <154279144+BrwnStallion@users.noreply.github.com>
2024-06-25 18:56:50 -0700

    feat: add conditional field/print render

--- src/components/General.jsx
+++ src/components/General.jsx
@@ -1,15 +1,54 @@
 import React from "react";
 import { useState } from "react";
 import { Input } from "./Input";
+import { InstanceButton } from "./Button";
 
 function GeneralInfo() {
   const [isCompleted, setIsCompleted] = useState(false);
+  const [generalInfo, setGeneralInfo] = useState({
+    name: "",
+    email: "",
+    phone: "",
+  });
+
+  let generalFields;
+  if (isCompleted) {
+    generalFields = (
+      <ul className="general-info">
+        <li>Name: {generalInfo.name}</li>
+        <li>Email: {generalInfo.email}</li>
+        <li>Phone Number: {generalInfo.phone}</li>
+      </ul>
+    );
+  } else {
+    generalFields = (
+      <div className="general-info">
+        <Input value={generalInfo.name} setValue={setGeneralInfo} type="name" />
+        <Input
+          value={generalInfo.email}
+          setValue={setGeneralInfo}
+          type="email"
+        />
+        <Input
+          value={generalInfo.phone}
+          setValue={setGeneralInfo}
+          type="phone"
+        />
+      </div>
+    );
+  }
   return (
-    <div className="general-info">
-      <Input type="name" />
-      <Input type="email" />
-      <Input type="phone" />
-    </div>
+    <>
+      {generalFields}
+      <div className="section-buttons">
+        <InstanceButton
+          type="completeEdit"
+          completeState={isCompleted}
+          setState={setIsCompleted}
+        />
+        <InstanceButton type="remove" />
+      </div>
+    </>
   );
 }
 
