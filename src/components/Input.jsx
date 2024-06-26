b71ece9f7937ed95e29a4b809d668f303f9b6562
Judah Almanzor <154279144+BrwnStallion@users.noreply.github.com>
2024-06-25 17:57:54 -0700

    feat: add switch for robust Input use cases
    Allow components to pass type, subType, parentSection props to Inputs.

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,11 +1,76 @@
 import React from "react";
 import { useState } from "react";
 
-function Input({ type }) {
+function Input({ type, subType, parentSection }) {
+  /* 
+  type: name, email, phone, text, date, checkbox
+  subType: school, degreeType, degreeName, employer, position, resp, startDate,
+  endDate
+  parentSection: school, work
+  */
+  let inputControl;
+  switch (type) {
+    case "name":
+      inputControl = (
+        <>
+          <label for="name">Full Name</label>
+          <input type="text" id="name" name="name"></input>
+        </>
+      );
+      break;
+    case "email":
+      inputControl = (
+        <>
+          <label for="email">Email</label>
+          <input type="email" id="email" name="email"></input>
+        </>
+      );
+      break;
+    case "phone":
+      inputControl = (
+        <>
+          <label for="phone">Phone Number</label>
+          <input type="tel" id="phone" name="phone"></input>
+        </>
+      );
+      break;
+    case "text":
+      inputControl = (
+        <>
+          <label for={subType}></label>
+          <input type="text" id={subType} name={subType}></input>
+        </>
+      );
+      break;
+    case "date":
+      inputControl = (
+        <>
+          <label for={subType}>
+            {subType === "startDate" ? "Start Date" : "Completion Date"}
+          </label>
+          <input type="text" id={subType} name={subType}></input>
+        </>
+      );
+      break;
+    case "checkbox":
+      inputControl = (
+        <>
+          <input
+            type="checkbox"
+            id="current"
+            name="current"
+            value="current"
+          ></input>
+          <label for="current">
+            Current {parentSection === "school" ? "School" : "Employer"}
+          </label>
+        </>
+      );
+      break;
+  }
   return (
     <>
-      <label>Test</label>
-      <input type="text"></input>
+      <div className={"input " + type}>{inputControl}</div>
     </>
   );
   /* 
