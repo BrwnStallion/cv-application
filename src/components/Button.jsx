304d635d317b0be88eef7cc8c22c735d31f626c1
Judah Almanzor <154279144+BrwnStallion@users.noreply.github.com>
2024-06-26 23:52:47 -0700

    feat: add onClick logic for add instanceId

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 function InstanceButton({ type, completeState, handleClick }) {
   /* 'complete'/'edit', 'remove', 'add new' buttons */
@@ -20,7 +21,8 @@ function InstanceButton({ type, completeState, handleClick }) {
     if (e.target.className === "completeEdit") {
       handleClick((prevState) => !prevState);
     } else if (e.target.className === "addNew") {
-      // add logic for adding new instance, using passed down set function
+      // add new instance, using passed-down set function
+      handleClick((prevIds) => [...prevIds, uuidv4()]);
     }
   };
 
