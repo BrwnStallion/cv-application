c6bf714f871bce32c898230a1c4b929f61f64563
Judah Almanzor <154279144+BrwnStallion@users.noreply.github.com>
2024-06-26 18:05:45 -0700

    feat: add info state obj; ul output condition

--- src/components/School.jsx
+++ src/components/School.jsx
@@ -2,20 +2,48 @@ import React from "react";
 import { useState } from "react";
 import { Input } from "./Input";
 import { InstanceButton } from "./Button";
+import { v4 as uuidv4 } from "uuid";
 
 function SchoolInstance() {
   const [isCompleted, setIsCompleted] = useState(false);
+  const [schoolExp, setSchoolExp] = useState([
+    {
+      id: uuidv4(),
+      schoolName: "",
+      degreeType: "",
+      degreeName: "",
+      dateGrad: "",
+      currentStatus: "",
+    },
+  ]);
 
   let schoolFields;
-  schoolFields = (
-    <div className="school-instance">
-      <Input type="text" subType="school" parentSection="school" />
-      <Input type="text" subType="degreeType" parentSection="school" />
-      <Input type="text" subType="degreeName" parentSection="school" />
-      <Input type="date" subType="endDate" parentSection="school" />
-      <Input type="checkbox" parentSection="school" />
-    </div>
-  );
+  if (isCompleted) {
+    schoolFields = schoolExp.map((school) => {
+      return (
+        <ul key={school.id} className="school-exp">
+          <li>School Name: {school.schoolName}</li>
+          <li>Degree Type: {school.degreeType}</li>
+          <li>Degree Name: {school.degreeName}</li>
+          <li>
+            {school.currentStatus === "current"
+              ? "Currently enrolled"
+              : "End Date" + school.dateGrad}
+          </li>
+        </ul>
+      );
+    });
+  } else {
+    schoolFields = (
+      <div className="school-instance">
+        <Input type="text" subType="school" parentSection="school" />
+        <Input type="text" subType="degreeType" parentSection="school" />
+        <Input type="text" subType="degreeName" parentSection="school" />
+        <Input type="date" subType="endDate" parentSection="school" />
+        <Input type="checkbox" parentSection="school" />
+      </div>
+    );
+  }
 
   return (
     <>
