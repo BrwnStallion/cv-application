d20161cb8f853e4d1635a244a249b174eb30cc48
Judah Almanzor <154279144+BrwnStallion@users.noreply.github.com>
2024-06-27 09:16:26 -0700

    feat: add add/remove props/state

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -2,18 +2,27 @@ import React from "react";
 import { useState } from "react";
 import { Input } from "./Input";
 import { InstanceButton } from "./Button";
+import { v4 as uuidv4 } from "uuid";
 
-function WorkInstance() {
+function WorkInstance({ id, handleRemove }) {
   const [isCompleted, setIsCompleted] = useState(false);
+  const [workExp, setWorkExp] = useState({
+    employerName: "",
+    positionName: "",
+    resp: [""],
+    endDate: "",
+    currentStatus: "",
+  });
 
+  let parentSection = "work";
   let workFields;
   if (isCompleted) {
     <p></p>;
   } else {
     workFields = (
       <div className="work-instance">
-        <Input type="text" subType="employer" parentSection="work" />
-        <Input type="text" subType="position" parentSection="work" />
+        <Input type="text" subType="employerName" parentSection="work" />
+        <Input type="text" subType="positionName" parentSection="work" />
         <Input type="date" subType="startDate" parentSection="work" />
         <Input type="date" subType="endDate" parentSection="work" />
         <Input type="checkbox" parentSection="work" />
@@ -30,18 +39,40 @@ function WorkInstance() {
           completeState={isCompleted}
           handleClick={setIsCompleted}
         />
-        <InstanceButton type="remove" />
+        <InstanceButton
+          type="remove"
+          instanceId={id}
+          handleClick={handleRemove}
+        />
       </div>
     </>
   );
 }
 
 function WorkSection() {
+  const [instIdList, setInstIdList] = useState([uuidv4()]);
   return (
     <>
-      <WorkInstance />
-      <InstanceButton type="addNew" />
+      {instIdList.map((id) => (
+        <WorkInstance key={id} id={id} handleRemove={setInstIdList} />
+      ))}
+      <InstanceButton type="addNew" handleClick={setInstIdList} />
     </>
   );
 }
+
+function RespSection() {
+  const [instIdList, setInstIdList] = useState([uuidv4()]);
+
+  return (
+    <>
+      {instIdList.map((id) => (
+        <RespInstance key={id} id={id} handleRemove={setInstIdList} />
+      ))}
+      <InstanceButton type="addNew" handleClick={setInstIdList} />
+    </>
+  );
+}
+
+function RespInstance() {}
 export { WorkInstance, WorkSection };
