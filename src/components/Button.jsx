72d708bcaf995950e9f6b816cd783e9d8dccb36c
Judah Almanzor <154279144+BrwnStallion@users.noreply.github.com>
2024-06-27 16:34:04 -0700

    feat: add item level add/remove; adjust props

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,13 @@ import React from "react";
 import { useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 
-function InstanceButton({ type, completeState, instanceId, handleClick }) {
+function InstanceButton({
+  type,
+  completeState,
+  instanceId,
+  handleClick,
+  context,
+}) {
   /* 'complete'/'edit', 'remove', 'add new' buttons */
   let text;
   switch (type) {
@@ -18,13 +24,42 @@ function InstanceButton({ type, completeState, instanceId, handleClick }) {
   }
 
   const onClick = (e) => {
-    if (e.target.className === "completeEdit") {
-      handleClick((prevState) => !prevState);
-    } else if (e.target.className === "addNew") {
-      // add new instance, using passed-down set function
-      handleClick((prevIds) => [...prevIds, uuidv4()]);
-    } else if (e.target.className === "remove") {
-      handleClick((prevIds) => prevIds.filter((id) => id !== instanceId));
+    switch (e.target.className) {
+      case "completeEdit":
+        handleClick((prevState) => !prevState);
+        break;
+
+      case "addNew":
+        if (context === "item") {
+          // for item level instance addition
+          handleClick((prevValues) => ({
+            ...prevValues,
+            resp: [
+              ...prevValues.resp,
+              {
+                id: uuidv4(),
+                content: "",
+              },
+            ],
+          }));
+        } else {
+          // for section level instance addition
+          handleClick((prevIds) => [...prevIds, uuidv4()]);
+        }
+        break;
+
+      case "remove":
+        if (context === "item") {
+          // for item level instance removal
+          handleClick((prevValues) => ({
+            ...prevValues,
+            resp: prevValues.resp.filter((item) => item.id !== instanceId),
+          }));
+        } else {
+          // for section level instance removal
+          handleClick((prevIds) => prevIds.filter((id) => id !== instanceId));
+        }
+        break;
     }
   };
 
